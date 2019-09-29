27195921dfa5d50d9c9f8fba540683b98c5da7d1
Hesham Salah <hs414h@gmail.com>
2019-09-29 04:57:50 +0200

    Moving space ship

--- game.js
+++ game.js
@@ -1,7 +1,22 @@
+var ship;
+
 function setup() {
   createCanvas(600, 600);
+  ship = new Spaceship(width/2, 570);
 }
 
 function draw() {
   background(0);
+  ship.show();
+}
+
+function keyPressed() {
+  switch(keyCode) {
+    case LEFT_ARROW:
+      ship.move('left');
+      break;
+    case RIGHT_ARROW:
+      ship.move('right');
+      break;
+  }
 }
--- spaceship.js
+++ spaceship.js
@@ -3,6 +3,19 @@ function Spaceship(xpos, ypos) {
   this.ypos = ypos;
 
   this.show = function () {
-    rect();
+    fill(255);
+    rectMode(CENTER);
+    rect(this.xpos, this.ypos, 50, 50);
+  }
+
+  this.move = function (direction) {
+    switch(direction) {
+      case 'left':
+        this.xpos -= 10;
+        break;
+      case 'right':
+        this.xpos += 10;
+        break;
+    }
   }
 }
